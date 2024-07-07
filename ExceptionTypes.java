cf154b45eda21a3c1023ba2ce0446d8cda1cca17
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-07 23:50:52 +0530

    Add files via upload

---
+++ ExceptionTypes.java
@@ -0,0 +1,24 @@
+//package com.example;
+
+public class ExceptionTypes {
+
+	public static void main(String[] args) {
+		//Arithmetic exception
+		double res=10/0;
+		//System.out.println(res);
+		
+		//ArrayIndexoutofBoundException
+		//int[] intArr=new int[5];
+		//intArr[5]=23;
+		
+		//NPE
+		//String str=null;
+		//str.concat("Hello");
+		
+		//NumberFormatException
+		Integer a=Integer.parseInt("a");
+		
+		
+	}
+
+}
